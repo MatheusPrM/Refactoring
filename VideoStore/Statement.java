4b0f026bdfbafeb5fb2f52ffe208af7afc44c952
Matheus Prado Miranda <matheuspradomiranda@MacBook-Pro-de-Matheus.local>
2022-06-25 00:13:41 -0300

    Commit 17

--- VideoStore/Statement.java
+++ VideoStore/Statement.java
@@ -1,3 +1,17 @@
+import java.util.Enumeration;
+
 public class Statement {
+
+   public String value(Customer aCustomer) {
+    Enumeration rentals = aCustomer.getRentals();
+    String result = getPrologue(aCustomer);
+
+    while (rentals.hasMoreElements()) {
+      Rental each = (Rental) rentals.nextElement();
+      result += getFigures(each);
+    }
+    result += getEpilogue(aCustomer);
+    return result;
+  }
 
 }
